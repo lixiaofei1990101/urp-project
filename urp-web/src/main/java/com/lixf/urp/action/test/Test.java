01fc00666da740fdde725f6d4bdf3b9758492618
lixiaofei1990101 <lixfadmin@163.com>
2016-09-12 15:41:24 +0800

    测试

---
+++ urp-web/src/main/java/com/lixf/urp/action/test/Test.java
@@ -0,0 +1,29 @@
+/*
+ * COPYRIGHT. ShenZhen MinSiDa Technology Co., Ltd. 2016.
+ * ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system, or transmitted,
+ * on any form or by any means, electronic, mechanical, photocopying, recording, 
+ * or otherwise, without the prior written permission of ShenZhen MinSiDa Technology Co., Ltd.
+ *
+ * Amendment History:
+ * 
+ * Date                   By              Description
+ * -------------------    -----------     -------------------------------------------
+ * 2016年9月12日    hfgff         Create the class
+*/
+
+package com.lixf.urp.action.test;
+
+/**
+ * @FileName Test.java
+ * @Description: 
+ *
+ * @Date 2016年9月12日 
+ * @author hfgff
+ * @version 1.0
+ * 
+ */
+public class Test {
+
+}
